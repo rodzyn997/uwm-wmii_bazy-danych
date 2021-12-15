43db67d5cbfa401f9bb3ad73815df4fb48297788
rodzyn997 <92019697+rodzyn997@users.noreply.github.com>
2021-12-15 11:13:47 +0100

    pracownicy

---
+++ SQL/lab_08.sql
@@ -0,0 +1,32 @@
+#=====================
+# Lab_08(firma)
+#=====================
+
+# 1
+
+select nazwisko from pracownik order by nazwisko ASC;
+
+# 2
+
+
+# 3
+
+select imie, nazwisko from pracownik where pensja between 3500 and 5000;
+
+# 4
+
+select towar,ilosc from stan_magazynowy where ilosc > 10;
+
+# 5
+
+#6
+
+select id_klienta, czy_firma from klient where czy_firma = 0 ;
+
+#7
+
+select data_zamowienia from zamowienie order by data_zamowienia Desc ;
+
+# 8
+
+
